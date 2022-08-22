4a8636e86557d11e71fdbee22b443d3dff9d6e93
mehedicse133 <mehedicse047@gmail.com>
2022-08-22 11:36:58 +0600

    intial commit

---
+++ oops_basic.py
@@ -0,0 +1,75 @@
+# Define a class
+class Person:
+
+    # class attributes
+    person = []
+    total = 0
+
+    # constractor method
+    def __init__(self) -> None:
+
+        # instance attributes
+        self.name = None
+        self.__age = None
+        self.__adult = None
+        self.person.append(self)
+
+    # gretter and public method
+    def get_name(self):
+        return self.name
+
+    # setter and private mehtod
+    def __set_name(self, new_name):
+        self.name = new_name
+
+    # instance methods
+    def add(self, name, age):
+        self.name = name
+        self.__age = age
+        self.adult = self.__is_adult(age)
+
+    # property decorator
+    @property
+    def details(self):
+        return f"My name is {self.name}, I am {self.adult}."
+
+    # static method
+    @staticmethod
+    def __is_adult(age):
+        if type(age) == "str":
+            return print("ddkddkdk")
+        else:
+            if age > 18:
+                return "Adult"
+            return "Teen age"
+
+    # class method
+    @classmethod
+    def num_of_person(cls):
+        return len(cls.person)
+
+
+# p1 = Person()
+# p1.add("Mehedi", "uuu")
+# print(p1.adult)
+
+
+# person_list = Person.person
+# for i in person_list:
+#     print(i.details)
+
+
+# Person.num_of_person()
+
+# p1 = Person("Mehedi", 10)
+# p2 = Person("Mehedi", 15)
+# p3 = Person("Mehedi", 14)
+# p4 = Person("Mehedi", 24)
+# print(Person.num_of_person())
+# print(Person.is_adult(p1.age))
+
+# personlist = [p1, p2, p3, p4]
+# for i in personlist:
+#     print(i.deatils)
+
+
