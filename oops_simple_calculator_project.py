6d0d414e16953a4570101c30fc57ef698f6b0423
mehedicse133 <mehedicse047@gmail.com>
2022-08-24 10:45:03 +0600

    second commit

---
+++ oops_simple_calculator_project.py
@@ -0,0 +1,31 @@
+# Simple Calculator
+
+
+class Calculator:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def add(self):
+        return self.num1 + self.num2
+
+    def sub(self):
+        return self.num1 - self.num2
+
+    def mul(self):
+        return self.num1 * self.num2
+
+    def div(self):
+        try:
+            return round(self.num1 / self.num2, 3)
+        except ZeroDivisionError as e:
+            return print(e)
+
+
+if __name__ == "__main__":
+
+    calc = Calculator(1, 1)
+    print(calc.add())
+    print(calc.sub())
+    print(calc.mul())
+    print(calc.div())
